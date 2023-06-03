59def45a350e926d38598df51336135893ea0797
Frdcbn <86790432+Frdcbn@users.noreply.github.com>
2023-06-03 16:59:48 +0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='mrbadutmodul',
-    version='1.0',
+    version='1.1',
     author='MR.BADUT',
     author_email='aldigamerz289@gmail.com',
     description='modul saya',
