8f24f3ecd0f3c2a69447779896bdebcd32269f02
Frdcbn <86790432+Frdcbn@users.noreply.github.com>
2023-06-03 17:00:53 +0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ setup(
     author='MR.BADUT',
     author_email='aldigamerz289@gmail.com',
     description='modul saya',
-    url='https://github.com/Frdcbn/mrbadut_modul',
+    url='https://github.com/Frdcbn/mrbadutmodul',
     py_modules=['mrbadutmodul'],
     install_requires = ['requests', 'beautifulsoup4', 'cloudscraper', 'tqdm', 'pyfiglet','geocoder']
 
